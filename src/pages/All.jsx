6104a7570a79236b0d4e72608dbae2016adc5a1b
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-07-08 21:50:14 -0230

    more attempts to fix bugs

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -5,7 +5,7 @@ import libraryData from "../../data/dbtest.json"
 export default function All() {
     const fetchLib = async () => {
         const res = await fetch("http://localhost:5000/");
-        console.log(res.json());
+        console.log(res);
         if (!res.ok) {
             throw new Error("Response was not ok");
         }
@@ -29,7 +29,7 @@ export default function All() {
     //     return 0;
     // });
 
-    // return data.map((book) => (
-    //     <CardContainer {...book} key={book.id} />
-    // ))
+    return data.map((book) => (
+        <CardContainer {...book} key={book.id} />
+    ))
 }
