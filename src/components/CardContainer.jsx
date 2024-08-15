66d87b5c06687455d3326f3d47c140b08a8faa17
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-08-14 22:34:18 -0230

    Fetch is working, but broke the card container, need to fix

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function CardContainer({ title, description, author, isbn }) {
+export default function CardContainer({ title, description, author, isbn, bookImage }) {
     // console.log(title, description);
-    let bookImage = "https://s3proxy.cdn-zlib.se//covers299/collections/userbooks/3588aa5c63b7304c305b4801cb3a17313e2edb82d5b8dae75ec4bf328ff76991.jpg"
+    let defaultBookImage = "https://s3proxy.cdn-zlib.se//covers299/collections/userbooks/3588aa5c63b7304c305b4801cb3a17313e2edb82d5b8dae75ec4bf328ff76991.jpg"
     return (
         <div className="card-container">
-            <img src={bookImage}></img>
+            <img src={bookImage !== "" ? bookImage : defaultBookImage}></img>
             <div className="card-container__info">
                 <h3 className="title">{title}</h3>
                 <p>{author}</p>
--- src/pages/ManualISBNSearch.jsx
+++ src/pages/ManualISBNSearch.jsx
@@ -1,17 +1,50 @@
 import { useState } from "react";
+import CardContainer from "../components/CardContainer"
 
 
 export default function ManualISBNSearch() {
-    const [isbn, setIsbn] = useState();
+    const [isbn, setIsbn] = useState("");
+    const [book, setBook] = useState(null);
+    const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
+
+    const searchForBook = async () => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            // console.log(data);
+
+            if (data.totalItems > 0) {
+                const book = data.items[0].volumeInfo;
+                // console.log(book);
+                setBook({
+                    title: book.title,
+                    author: book.authors[0],
+                    description: book.description,
+                    isbn: book.industryIdentifiers[0].identifier,
+                    coverImage: book.imageLinks.thumbnail
+                });
+            } else {
+                console.log("No book found");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+        console.log(book);
+    }
+
 
 
     return (
-        <div className="manual_isbn_entry_container">
-            <h3>Search By ISBN</h3>
-            <form>
-                <input type="number" id="isbn_search" name="isbn_search" placeholder="Enter ISBN"></input>
-                <button>Search</button>
-            </form>
-        </div>
+        <>
+            <div className="manual_isbn_entry_container">
+                <h3>Search By ISBN</h3>
+                <input type="number" value={isbn} id="isbn_search" name="isbn_search" placeholder="Enter ISBN" onChange={(e) => setIsbn(e.target.value)}></input>
+                <button onClick={searchForBook}>Search</button>
+            </div>
+
+            <CardContainer {...book} bookImage={book.coverImage} description={book.description.substring(0, 40) + "..."} ></CardContainer>
+        </>
+
     );
 }
