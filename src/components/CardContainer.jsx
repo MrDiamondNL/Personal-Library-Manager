d94bbb0ca960af750a09a44709e69e55cc25cfc0
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-08-18 10:00:46 -0230

    got the proper images working

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function CardContainer({ title, description, author, isbn, bookImage }) {
+export default function CardContainer({ title, description, author, isbn, coverImage }) {
     // console.log(title, description);
     let defaultBookImage = "https://s3proxy.cdn-zlib.se//covers299/collections/userbooks/3588aa5c63b7304c305b4801cb3a17313e2edb82d5b8dae75ec4bf328ff76991.jpg"
     return (
         <div className="card-container">
-            <img src={bookImage !== "" ? bookImage : defaultBookImage}></img>
+            <img src={coverImage !== "" ? coverImage : defaultBookImage}></img>
             <div className="card-container__info">
                 <h3 className="title">{title}</h3>
                 <p>{author}</p>
