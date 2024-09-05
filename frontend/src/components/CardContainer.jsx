b04ad467a3b068391222011a47eef17f295f6dfd
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-09-04 22:03:36 -0230

    more debugging

--- frontend/src/components/CardContainer.jsx
+++ frontend/src/components/CardContainer.jsx
@@ -1,8 +1,8 @@
 
 export default function CardContainer({ title, description, author, isbn, coverImage }) {
     // console.log(title, description);
-    let defaultBookImage = "https://s3proxy.cdn-zlib.se//covers299/collections/userbooks/3588aa5c63b7304c305b4801cb3a17313e2edb82d5b8dae75ec4bf328ff76991.jpg"
-    console.log(coverImage !== "" ? coverImage : defaultBookImage);
+    let defaultBookImage = "https://s3proxy.cdn-zlib.se//covers299/collections/userbooks/3588aa5c63b7304c305b4801cb3a17313e2edb82d5b8dae75ec4bf328ff76991.jpg";
+    // console.log(coverImage !== "" ? coverImage : defaultBookImage);
     return (
         <div className="card-container">
             <img src={coverImage !== undefined ? coverImage : defaultBookImage}></img>
