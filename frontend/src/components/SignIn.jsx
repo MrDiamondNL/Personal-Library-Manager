dcdf250c590bb351e64c8293b6e4d75e54b4b3e8
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-10-08 20:24:45 -0230

    fixing css

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -51,10 +51,15 @@ export default function SignIn() {
     return (
         <div className="sign-in-container">
             <form onSubmit={emailSignIn} id="sign-in-form">
-                <h1>Log In</h1>
-                <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                <button type="submit">Login</button>
+
+                <div className="detail_entry_container">
+                    <h1>Log In</h1>
+                    <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                    <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    <button type="submit">Login</button>
+                </div>
+
+
             </form>
             <GoogleButton onClick={handleGoogleSignIn} />
         </div>
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function SignUp() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const { emailSignUp } = useAuth();
+    const navigate = useNavigate();
 
     function handleEmailSignUp(e) {
         e.preventDefault();
@@ -14,6 +15,7 @@ export default function SignUp() {
         }).catch((error) => {
             console.log(error);
         });
+        navigate("/");
     }
 
     return (
