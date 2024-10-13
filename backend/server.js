5c5ac42453787eeae27cf1ad550b0ed83c42ac85
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-10-13 17:43:46 -0230

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ let activeDb = "library"
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  }));
 
 app.use((req, res, next) => {
     console.log(`${req.method} request received at ${req.url}`);
