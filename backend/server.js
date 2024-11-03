f8002901be0f130e29a16a16aa4a5be8f46f45e8
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2024-11-02 22:21:43 -0230

    more fixin

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.post("/library", (req, res) => {
 app.put("/lend", async (req, res) => {
     try {
         const updateItemID = new mongoose.Types.ObjectId(req.body.id);
-        const email = new req.body.email;
+        const email = req.body.email;
     
         console.log(`Attempting to find item with ID:${updateItemID} in database`);
 
