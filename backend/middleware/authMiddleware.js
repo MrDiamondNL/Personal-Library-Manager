99c9cf3523cafbf03036592ae588491c42491880
MrDiamondNL <36870588+MrDiamondNL@users.noreply.github.com>
2025-05-11 22:34:02 -0230

    trying to track down bug

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -50,6 +50,7 @@ const checkForFirebaseToken = async (req, res, next) => {
 const checkForCustomToken = async (req, res, next) => {
     const token = req.cookies.customToken;
     console.log(req.cookies);
+    console.log("did it get here");
     if (!token) {
         return next(CustomError.unauthorized("Authentication Required"));
     }
--- backend/tests/routes/basicRoutes.test.js
+++
@@ -1,9 +0,0 @@
-const Basic = require("../../models/Basic");
-
-jest.mock("../../models/Basic");
-
-test("This jesus constructor test", () => {
-    const testBasic = new Basic();
-
-    expect(Basic).toHaveBeenCalled();
-})
